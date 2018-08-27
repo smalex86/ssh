eede04223da9b5a19f2c7c8006a79e9c2fcfdce6
Смирнов Александр Павлович <smirnovap@kadastr.ru>
2018-08-27 18:01:12 +0700

    Обновлен файл теста

--- cli/ssh-test.php
+++ cli/ssh-test.php
@@ -7,18 +7,18 @@ use smalex86\ssh\Ssh;
 try {
     $ssh = new Ssh();
     $ssh->setLogger($logger);
-    $ssh->connect($configObj->access['server']);
-    $ssh->authPassword($configObj->access['user'], $configObj->access['password']);
-    $ssh->shellStart();
-    echo 'результат = ' . var_export($ssh->execShellCommand('sudo -i ' . PHP_EOL . $configObj->access['password']), true) . PHP_EOL;
-    echo 'результат = ' . var_export($ssh->execShellCommand('service --status-all'), true) . PHP_EOL;
-    if ($ssh->checkRootUser()) {
+    $ssh->openConnection($configObj->access['server'])->authPassword(
+            $configObj->access['user'], $configObj->access['password']);
+    $ssh->openShell();
+    echo 'результат = ' . var_export($ssh->shell->execCommand('sudo -i ' 
+            . PHP_EOL . $configObj->access['password']), true) . PHP_EOL;
+    echo 'результат = ' . var_export($ssh->shell->execCommand('service '
+            . '--status-all'), true) . PHP_EOL;
+    if ($ssh->shell->checkRootUser()) {
         echo 'Current user is root' . PHP_EOL;
     } else {
         echo 'Current user is not root' . PHP_EOL;
     }
-    $ssh->shellClose();
-    $ssh->disconnect();
 } catch (smalex86\ssh\exception\BaseSshException $bse) {
     echo 'Ошибка: ' . $bse->getMessage() . PHP_EOL;
 }
