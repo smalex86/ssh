afc2a5b9f3ce16a230b6071bf5b249fe6f8e7671
Смирнов Александр Павлович <smirnovap@kadastr.ru>
2018-08-28 18:09:09 +0700

    В sftp добавлен метод для отправки файлов на удаленную машину

--- src/Sftp.php
+++ src/Sftp.php
@@ -8,9 +8,19 @@ use smalex86\ssh\Connection;
 /**
  * Sftp
  *
+ * Сделать:
+ * 1. Получение файла с удаленной машины с выводом статуса
+ * 2. Отправка данных в файл на удаленной машине
+ * 
+ * Сделано:
+ * 1. Отправка файла на удаленную машину с выводом статуса
+ * 
  * @author smirnov
  */
 class Sftp implements LoggerAwareInterface {
+    
+    /** Размер порции данных при отправке/получении данных */
+    const DATA_LENGTH = 1024 * 200;
     
     use \Psr\Log\LoggerAwareTrait;    
     /**
@@ -53,11 +63,64 @@ class Sftp implements LoggerAwareInterface {
         }
     }
     
-    
+    /**
+     * Отправка файла на удаленную машину
+     * @param string $localFile
+     * @param string $remoteFile
+     * @return int количество переданных байт
+     * @throws exception\SftpException
+     * @throws Exception
+     */
     public function sendFile($localFile, $remoteFile) {
-        $streamPath = 'ssh2.sftp://' . $this->session . $remoteFile;
-        $this->logger->debug('Путь для открытия потока записи: ' . $streamPath);
-        //$stream = fopen(, $mode);
+        $localFileSize = filesize($localFile);
+        $readData = 0;
+        $writeData = 0;
+        $this->logger->debug(sprintf('Отправка файла %s на сервер в файл %s', 
+                $localFile, $remoteFile));
+        try {
+            $local = fopen($localFile, 'r');
+            $remote = fopen('ssh2.sftp://' . intval($this->session) //. '/.'
+                    . $remoteFile, 'w');
+            while (!feof($local)) {
+                // чтение данных
+                $rd = fread($local, self::DATA_LENGTH);
+                if ($rd === false) {
+                    throw new Exception('ошибка при чтении данных');
+                } elseif ($rd) {
+                    $readData += strlen($rd);
+                }
+                // отправка данных
+                $wd = fwrite($remote, $rd);
+                if ($wd === false) {
+                    throw new Exception('ошибка при отправке данных');
+                } else if ($wd) {
+                    $writeData += $wd;
+                }
+                $this->logger->debug(sprintf('прочитано %u/%u байт и отправлено '
+                        . '%u/%u байт.', $readData, $localFileSize, $writeData, 
+                        $localFileSize));
+            }
+            fclose($remote);
+            fclose($local);
+            // успех
+            $this->logger->debug(sprintf('Файл %s был отправлен в файл %s, было '
+                    . 'прочитано %u/%u байт и отправлено %u/%u байт.', 
+                    $localFile, $remoteFile, $readData, $localFileSize, 
+                    $writeData, $localFileSize));
+            return $writeData;
+        } catch (\Exception $e) {
+            if (isset($local) && is_resource($local)) {
+                fclose($local);
+            }
+            if (isset($remote) && is_resource($remote)) {
+                fclose($remote);
+            }
+            throw new exception\SftpException('При отправке данных из файла %s '
+                    . 'в файл %s возникла ошибка, было отправлено %s байт из %s.'
+                    . ' Ошибка: %s (line:%u)', $localFile, $remoteFile, 
+                    $writeData, $localFileSize, $e->getMessage(), 
+                    $e->getLine());
+        }
     }
     
 }
